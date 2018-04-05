d7de9bbfeafd37f9ebedf8a4e8cfb3a5b9466274
Uchio KONDO <udzura@pepabo.com>
2018-04-05 17:16:10 +0900

    Make a echo server with non-blocking accept

--- lib/marfusha/daemon.rb
+++ lib/marfusha/daemon.rb
@@ -1,26 +1,73 @@
-require 'serverengine'
+require "serverengine"
+require "celluloid/io"
 
 module Marfusha
-  module Publisher
+  module PublisherProvisioner
     def before_run
-      @sock = TCPServer.new(config[:bind], config[:port])
+    end
+  end
+
+  class PublisherServer
+    include Celluloid::IO
+    finalizer :finalize
+    def initialize(host, port)
+      puts "*** Starting echo server on #{host}:#{port}"
+
+      # Since we included Celluloid::IO, we're actually making a
+      # Celluloid::IO::TCPServer here
+      @server = TCPServer.new(host, port)
+      async.run
     end
 
-    attr_reader :sock
+    def finalize
+      @server.close if @server
+    end
+
+    def run
+      loop { async.handle_connection @server.accept }
+    end
+
+    def handle_connection(socket)
+      _, port, host = socket.peeraddr
+      puts "*** Received connection from #{host}:#{port}"
+      socket.write "I am publisher!!1"
+      loop do
+        p = socket.readpartial(4096)
+        socket.write p
+        if p.end_with? "\n"
+          puts "*** #{host}:#{port} closed connection"
+          socket.close
+          break
+        end
+      end
+    rescue EOFError
+      puts "*** #{host}:#{port} disconnected"
+      socket.close
+    end
   end
 
   module PublisherWorker
+    def initialize
+      require "celluloid/autostart"
+    end
+
     def run
-      until @stop
-        # you should use Cool.io or EventMachine actually
-        c = server.sock.accept
-        c.write "This is publisher!\n"
-        c.close
+      puts "Run Server!!!1"
+      @runner = PublisherServer.new(config[:bind], config[:port])
+      @alive = true
+      puts "Server is now listening"
+      while @alive
+        sleep 0.01
       end
     end
 
     def stop
-      @stop = true
+      puts "Accept stop"
+      @runner.terminate if @runner
+    rescue ::Celluloid::DeadActorError => e
+      puts "Detect error #{e.inspect} and skip"
+    ensure
+      @alive = false
     end
   end
 
@@ -56,13 +103,13 @@ when "publisher"
     log: '/tmp/pub.log',
     pid_path: '/tmp/pub.pid',
     worker_type: 'process',
-    workers: 4,
+    workers: 1,
     bind: '0.0.0.0',
     port: 8310,
     server_process_name: 'marfusha: publisher',
     worker_process_name: 'marfusha: publisher worker',
   }
-  eng = ServerEngine.create(Marfusha::Publisher, Marfusha::PublisherWorker, options)
+  eng = ServerEngine.create(Marfusha::PublisherProvisioner, Marfusha::PublisherWorker, options)
   eng.run
 when "subscriber"
   options = {
@@ -71,7 +118,7 @@ when "subscriber"
     log: '/tmp/sub.log',
     pid_path: '/tmp/sub.pid',
     worker_type: 'process',
-    workers: 4,
+    workers: 1,
     bind: '0.0.0.0',
     port: 8311,
     daemon_process_name: 'marfusha: subscriber',
